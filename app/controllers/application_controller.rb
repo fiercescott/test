3761c17f110a8c87bafeff03a76f9c0fe6381c6e
fiercescott <colliers136@gmail.com>
2017-10-01 17:00:45 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hello World"
+    render html: "This is the git test. I edited this to ensure it was recieveing from git"
   end
 end
